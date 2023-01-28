f181955396169bb58ec6480d874ac53bbfcea91e
mulybirish <mulugata13@gmail.com>
2023-01-28 14:25:31 +0100

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,9 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <h1>Hello world</h1>
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
+        <h1>Hello world</h1>
       </main>
     </>
   );
